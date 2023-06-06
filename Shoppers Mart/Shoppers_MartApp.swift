4e610d0b629941af428515cfdba2fb411b6c2cf1
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-06-06 22:33:54 +0600

    Project created.

---
+++ Shoppers Mart/Shoppers_MartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoppers_MartApp.swift
+//  Shoppers Mart
+//
+//  Created by Md Abir Hossain on 06-06-2023.
+//
+
+import SwiftUI
+
+@main
+struct Shoppers_MartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
